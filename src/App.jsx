1525847f9ade7a398067d258a15c34a0ce730558
Ivan <152219975+IvanOjedaOjeda@users.noreply.github.com>
2024-05-13 12:08:12 -0400

    commits 5 Newsletterlisto

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,26 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import NewsletterSection from './components/NewsletterSection';
 
 function App() {
   return (
     <div>
-      <Header />
+      <Header />       
+        <NewsletterSection />      
       <Footer />
-      
-    </div>
+    </div >
+  );
+}
+
+function Menu() {
+  return (
+    <ul>
+      <li>Plato 1</li>
+      <li>Plato 2</li>
+      <li>Plato 3</li>
+      {/* Agrega más elementos <li> según sea necesario para los platos de tu menú */}
+    </ul>
   );
 }
 
---
+++ src/components/NewsletterSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NewsletterSection.css'; // Importa el archivo CSS para los estilos de la sección
+
+const NewsletterSection = () => {
+  return (
+    <section className="newsletter-section">
+      <h2>Suscríbete a nuestro Newsletter</h2>
+      <form>
+        <input type="email" placeholder="Ingresa tu correo electrónico" />
+        <button type="submit">Suscribirse</button>
+      </form>
+    </section>
+  );
+}
+
+export default NewsletterSection;
